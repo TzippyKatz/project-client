ce275ad58d445148c77a11251d9f43f07e11e508
TzippyKatzGithub <zk6722355@gmail.com>
2025-03-09 22:41:24 +0200

    begin diet compinnent - itreturns desc goals of all diets but not add a new diet because the token

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import PATHS from "./../src/routes/paths";
 import { HomePage } from './layouts/Home';
 import { LoginPage } from './layouts/Login';
+import { DietPage } from './layouts/Diet';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       {/* <HomePage /> */}
       <LoginPage />
+      {/* <DietPage /> */}
     </div>
   );
 }
--- src/services/diets.service.ts
+++ src/services/diets.service.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from "axios";//
+import axios, { AxiosResponse } from "axios";//
 import axiosInstance from "./axios";//
 import { dietType } from "../types/diet.type"
 import { baseUrl } from "./axios"
@@ -7,32 +7,46 @@ const serviceUrl = `${ baseUrl }/Diet`
 
 export const getDiet = async () =>{
     //
-    const response = await axiosInstance.get(serviceUrl)
+    const response = await axios.get(serviceUrl)
     const data = await response.data
     return data
 }
 
 export const getDietById = async (diet: Omit<dietType, 'id'>) =>{
-    const response = await axiosInstance.get(serviceUrl)
+// export const getDietById = async (diet: dietType) =>{
+    const response = await axios.get(serviceUrl)
     const data = await response.data
     return data
 }
 
 //אני כן מקבלת כרגע מזהה אבל אני צריכה לשנות את זה
-export const addTypeMeal = async (diet: Omit<dietType, 'id'>) =>{
-    const response = await axiosInstance.post(serviceUrl, diet)
+export const addDiet = async (diet: Omit<dietType, 'id'>) =>{
+// export const addDiet = async (diet: dietType) =>{
+    const token = localStorage.getItem('token')
+    
+    const response = await axios.post(serviceUrl, null, {
+        params: {
+            DietitianId: diet.DietitianId,
+            descGoal: diet.descGoal,
+            meals: diet.meals,
+            rate: diet.rate
+        },
+        headers: {
+            Authorization: `Bearer ${token}` // הוספת הטוקן לכותרת
+        }
+    })
     const data = response.data
     return data
 }
 
-export const deleteTypeMeal = async (id: number) =>{
-    const response = await axiosInstance.delete(`${serviceUrl}/${id}`)
+export const deleteDiet = async (id: number) =>{
+    const response = await axios.delete(`${serviceUrl}/${id}`)
     const data = response.data
     return data
 }
 
-export const updateTypeMeal = async (diet: dietType) =>{
-    const response = await axiosInstance.put(`${serviceUrl}/${diet.id}`, diet)
+export const updateDiet = async (diet: dietType) =>{
+    const response = await axios.put(`${serviceUrl}/${diet.id}`, diet)
     const data = response.data
     return data
 }
